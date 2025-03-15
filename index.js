a3666bc179cf7821d7aae0514d88b997c0eee66c
Uniqugirl <dmanjulareddy23@gmail.com>
2025-03-15 10:57:45 +0530

    Add files via upload
    Sir, this are my web development project

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log('hello world');
+// let name="manjula";
+// let age="19";
+// console.log(name);
+// console.log(age);
+// let firstname="D.n ";
+// let lastname="manjula";
+// name=firstname+lastname;
+// console.log(name);
+// let name="manjula";
+// let age="19";
+// let isApproved=false;
+// let firstname=undefined;
+// let selectedcolor=null;
+// let person={
+//     name:"saraswathi",
+//     age:40
+// };
+// console.log(person)
+// person.name="vani"
+// person['age']=43
+// console.log(person);
+// console.log(person.name); 
+// console.log(person.age);
+// let selectedfruits=["apple","orange","grapes"];
+//     selectedfruits[3]='watermelon';
+// console.log(selectedfruits);
+// console.log(selectedfruits.length);
+// function square(number){
+//     return number*number;
+// }
+// function cube(number){
+//     return number*number*number;
+// }
+// console.log(square(6));
+// console.log(cube(5));
+// function addition(number){
+//     return number+number;
+// }
+// console.log(addition(8))
+ window.alert("your 50% data is over");
+ document.getElementById("myh1").textcontent="hello"
+ document.getElementById("myp").textcontent="i like Js"
